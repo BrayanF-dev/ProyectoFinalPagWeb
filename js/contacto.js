ab014a2341a0be3550d2f6dc6bfa01824be9231c
CristianCastro0 <cccastelblanco@ucompensar.edu.co>
2025-05-27 22:40:00 -0500

    Update contacto.js

--- js/contacto.js
+++ js/contacto.js
@@ -1,1 +1,8 @@
-
+window.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('contactForm');
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+    alert('Mensaje enviado. ¡Muchas gracias por contactarnos!');
+    form.reset();
+  });
+});
